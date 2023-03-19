af71957bf3712a9a5eaf3044b0737ee32e809682
theertha_avnsh <theerthaavnsh@gmail.com>
2023-03-19 14:40:15 +0530

    removedbackend

--- Backend/routes/user-routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import {signup, login } from '../controllers/user-controller.js'
-
-const userRouter = express.Router()
-
-userRouter.post('/signup', signup)
-userRouter.post('/login', login)
-
-export default userRouter;
